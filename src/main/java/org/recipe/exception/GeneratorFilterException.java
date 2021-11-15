543547c4114802d1949460af0886981037d97bca
nikolavojicic <nikolavojicic@outlook.com>
2021-11-15 19:45:51 +0100

    Add custom exception for Generator#filter

---
+++ src/main/java/org/recipe/exception/GeneratorFilterException.java
@@ -0,0 +1,9 @@
+package org.recipe.exception;
+
+public class GeneratorFilterException extends RuntimeException {
+
+    public GeneratorFilterException(String message) {
+        super(message);
+    }
+
+}
